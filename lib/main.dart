b1e0b3c7cb2fa2d8318676f339e4465e7e85f004
abinpj <abinpj007wol@gmail.com>
2020-08-20 13:33:26 +0530

    except-optab

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -34,18 +35,21 @@ class _MyHomePageState extends State<MyHomePage> {
   double val2 = 0.0;
   var cur1 = 'INR';
   var cur2 = 'USD';
-  double convert() {
+
+  void convert() {
     var url = "https://api.exchangeratesapi.io/latest?base=$cur1";
     http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"}).then(
         (r) {
       Map body = json.decode(r.body);
-      double temp2 = body['rates'].cur2;
-      val2 = val1 * temp2;
+      val2 = body['rates'][cur2];
+      val2 = val1 * val2;
     });
-    return val2;
   }
 
+  void send() {}
+
   TextEditingController _controller = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,88 +61,104 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Row(children: <Widget>[
-              SizedBox(
-                width: 100,
-                child: TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    labelText: "Input Value",
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: "Input Value",
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            val1 = double.parse(newValue);
+                          });
+
+                          convert();
+                        }),
                   ),
-                ),
-              ),
-              DropdownButton<String>(
-                  value: cur1,
-                  icon: Icon(Icons.arrow_downward),
-                  iconSize: 24,
-                  elevation: 16,
-                  style: TextStyle(color: Colors.teal),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.tealAccent,
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      cur1 = newValue;
-                    });
-                  },
-                  items: <String>[
-                    'CAD',
-                    'HKD',
-                    'ISK',
-                    'PHP',
-                    'DKK',
-                    'HUF',
-                    'CZK',
-                    'GBP',
-                    'RON',
-                    'SEK',
-                    'IDR',
-                    'INR',
-                    'BRL',
-                    'RUB',
-                    'HRK',
-                    'JPY',
-                    'THB',
-                    'CHF',
-                    'EUR',
-                    'MYR',
-                    'BGN',
-                    'TRY',
-                    'CNY',
-                    'NOK',
-                    'NZD',
-                    'ZAR',
-                    'USD',
-                    'MXN',
-                    'SGD',
-                    'AUD',
-                    'ILS',
-                    'KRW',
-                    'PLN'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList()),
-            ]),
+                  DropdownButton<String>(
+                      value: cur1,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: TextStyle(color: Colors.teal),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.tealAccent,
+                      ),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          cur1 = newValue;
+                        });
+                        convert();
+                      },
+                      items: <String>[
+                        'CAD',
+                        'HKD',
+                        'ISK',
+                        'PHP',
+                        'DKK',
+                        'HUF',
+                        'CZK',
+                        'GBP',
+                        'RON',
+                        'SEK',
+                        'IDR',
+                        'INR',
+                        'BRL',
+                        'RUB',
+                        'HRK',
+                        'JPY',
+                        'THB',
+                        'CHF',
+                        'EUR',
+                        'MYR',
+                        'BGN',
+                        'TRY',
+                        'CNY',
+                        'NOK',
+                        'NZD',
+                        'ZAR',
+                        'USD',
+                        'MXN',
+                        'SGD',
+                        'AUD',
+                        'ILS',
+                        'KRW',
+                        'PLN'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()),
+                ]),
             FloatingActionButton(
+              child: Icon(IconData(57539, fontFamily: "MaterialIcons")),
               onPressed: () {
                 _controller.text = val2.toString();
                 val1 = val2;
-                val2 = val1;
+                convert();
               },
             ),
-            Row(children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
               SizedBox(
                 width: 100,
                 child: TextField(
-                  controller: _controller,
+                  controller: _controller2,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      val2 = double.parse(newValue);
+                    });
+                  },
                   decoration: InputDecoration(
-                    labelText: "Output Value",
+                    labelText: val2.toString(),
                   ),
                 ),
               ),
